451a053d894bcd6dec347744869b6bdf4c295a47
Nguyen Manh Cuong <cuongnm@younetgroup.com>
2021-10-28 18:54:23 +0700

    fix: Show incorrect time on android when open time picker

--- src/datetimepicker.android.js
+++ src/datetimepicker.android.js
@@ -67,8 +67,7 @@ function getPicker({
 function timeZoneOffsetDateSetter(date, timeZoneOffsetInMinutes) {
   let localDate = date;
   if (typeof timeZoneOffsetInMinutes === 'number') {
-    const offset =
-      localDate.getTimezoneOffset() * MIN_MS + timeZoneOffsetInMinutes * MIN_MS;
+    const offset = timeZoneOffsetInMinutes * MIN_MS;
     localDate = new Date(date.getTime() - offset);
   }
   return localDate;
@@ -121,13 +120,12 @@ export default function RNDateTimePicker(props: AndroidNativeProps) {
           switch (action) {
             case DATE_SET_ACTION:
               date.setFullYear(year, month, day);
-              date = timeZoneOffsetDateSetter(date, timeZoneOffsetInMinutes);
               event.nativeEvent.timestamp = date;
               onChange(event, date);
               break;
 
             case TIME_SET_ACTION:
-              date.setHours(hour, minute);
+              date.setUTCHours(hour, minute);
               date = timeZoneOffsetDateSetter(date, timeZoneOffsetInMinutes);
               event.nativeEvent.timestamp = date;
               onChange(event, date);
